249d82d5ee100d3cccda4c386ceb7a4b3f0d7cc8
amagnies <adrien.magnies@hotmail.fr>
2023-08-25 10:47:03 +0200

    Intégration du composant Linechart.js

--- src/components/charts/Linechart/Linechart.js
+++ src/components/charts/Linechart/Linechart.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer, Rectangle } from 'recharts';
 import './linechart.css'
 
 
@@ -7,11 +7,42 @@ const Linechart = ({ infoSessions }) => {
 
   const data = infoSessions.sessions;
 
+  const formatDayOfWeek = (day) => {
+    const daysOfWeek = ["L", "M", "M", "J", "V", "S", "D"];
+    return daysOfWeek[day - 1];
+  };
+
+  const CustomTooltip = ({ active, payload }) => {
+    if (active && payload && payload.length) {
+      return (
+        <div className={`linechart-tooltip ${active ? "active" : ""}`}>
+          <p className="desc">{`${payload[0].value} min`}</p>
+        </div>
+      );
+    }
+
+    return null;
+  };
+
+  const CustomCursor = ({ points, width }) => {
+    return (
+      <Rectangle
+        fill="#000000"
+        opacity={0.1}
+        x={points[1].x}
+        width={width}
+        height={288}
+      />
+    );
+  };
+  
+
     return (
         <div className="dashboard__linechart">
             <ResponsiveContainer width="100%" height="100%">
                 <LineChart
                     data={data}
+                    margin={{ top: 90, right: 0, left: 0, bottom: 5 }}
                 >
                 <CartesianGrid
                     strokeDasharray=""
@@ -20,19 +51,24 @@ const Linechart = ({ infoSessions }) => {
                 />
                 <XAxis
                   dataKey="day"
+                  tickFormatter={formatDayOfWeek}
                   axisLine={false}
                   tickLine={false}
                   tickMargin={0}
-                  tick={{ fill: '#FFFFFF', opacity: '0.5' }}
+                  tick={{ fill: '#FFFFFF', opacity: '0.5', fontSize: '12' }}
                   padding={{ left: 16, right: 16 }}
                 />
                 <YAxis
                   axisLine={false}
                   tickLine={false}
                   type="number"
-                  hide="true"
+                  hide="true" 
                 />
-                <Tooltip />
+                <Tooltip //infobulle
+                  content={<CustomTooltip />}
+                  position={{ y: 64 }}
+                  cursor={<CustomCursor />}
+                />
                 <Line
                   type="natural"
                   dataKey="sessionLength"
@@ -41,16 +77,16 @@ const Linechart = ({ infoSessions }) => {
                   dot={false}
                 />
                 <text
-                  x={50}
-                  y={50}
+                  x={32}
+                  y={32}
                   fontSize={15}
                   textAnchor="start"
                   fill="rgba(255, 255, 255, 0.5)"
                 >
-                  <tspan x={40} dy="0">
+                  <tspan x={24} dy="0">
                     Durée moyenne
                   </tspan>
-                  <tspan x={40} dy="1.2em">
+                  <tspan x={24} dy="1.5em">
                     des sessions
                   </tspan>
                 </text>
