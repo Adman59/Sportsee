d57461b7cee6aee36daef58c7ecf740343ab6d70
amagnies <adrien.magnies@hotmail.fr>
2023-09-12 11:47:12 +0200

    Retour à un état ou l'appel à l'API via axios est fonctionnel avant de corriger pour répondre au dernier critère du projet

--- src/_services/axiosAPI.js
+++ src/_services/axiosAPI.js
@@ -1,74 +1,37 @@
 import axios from "axios";
-import { BarChartModel } from "@/models/BarChart_Model";
-import { RadialChartModel } from "@/models/RadialChart_Model";
-import { LineChartModel } from "@/models/LineChart_Model";
-import { RadarChartModel } from "@/models/RadarChart_Model";
 
 export const getUserDataFromApi = async (userId) => {
     try {
         const response = await axios.get(`http://localhost:3000/user/${userId}`);
-
-        if(response.ok){
-            const data = await response.json();
-            console.log(data);
-            if(data){
-                return new RadialChartModel(data.data);
-            }
-        }else{
-            throw new Error('true');
-        }
-
+        return response.data.data;
     } catch (error) {
-        return false
+        console.error(error);
     }
 };
 
 export const getUserActivityDataFromApi = async (userId) => {
     try {
         const response = await axios.get(`http://localhost:3000/user/${userId}/activity`);
-        if(response.ok){
-            const data = await response.json();
-            if(data){
-                return new BarChartModel(data.data)
-            }
-        }else{
-            throw new Error('true');
-        }
+        return response.data.data;
     } catch (error) {
-        return false
+        console.error(error);
     }
 };
 
 export const getUserSessionsDataFromApi = async (userId) => {
     try {
         const response = await axios.get(`http://localhost:3000/user/${userId}/average-sessions`);
-
-        if(response.ok) {
-            const data = await response.json();
-            if (data) {
-                return new LineChartModel(data.data)
-            }
-        } else {
-            throw new Error("true");
-        }
+        return response.data.data;
     } catch (error) {
-        return false
+        console.error(error);
     }
 };
 
 export const getUserPerformanceDataFromApi = async (userId) => {
     try {
         const response = await axios.get(`http://localhost:3000/user/${userId}/performance`);
-        
-        if(response.ok) {
-            const data = await response.json();
-            if(data) {
-                return new RadarChartModel(data.data)
-            }
-        } else {
-            throw new Error("true");
-        }
+        return response.data.data;
     } catch (error) {
-        return false
+        console.error(error);
     }
 };
